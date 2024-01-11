55fec6640cbe67603b316f701f84567fc824d626
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-01-10 21:52:32 -0300

    useCases-designPatterns - feat: created the pattern useCase and Repository to Users

---
+++ src/use-cases/register.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { UsersRepository } from '@/repositories/users-repository';
+import { hash } from 'bcryptjs';
+
+interface RegisterUseCasesRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export class RegisterUseCase {
+	constructor(private usersRepository: UsersRepository) {}
+
+	async execute({ name, email, password }: RegisterUseCasesRequest) {
+		const password_hash = await hash(password, 6);
+    
+		const userWithSameEmail = await this.usersRepository.findByEmail(email);
+    
+		if (userWithSameEmail) {
+			throw new Error('E-mail already exists.');
+		}
+    
+		//const prismaUsersRepository = new PrismaUsersRepository();
+    
+		await this.usersRepository.create({
+			name,
+			email,
+			password_hash
+		});
+	}
+}
+
