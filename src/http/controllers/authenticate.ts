1d299295de6051b6600b390b629a837cba3ed620
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-01-27 11:18:57 -0300

    controllers-tests-e2e - feat: created flow to generate the token upon authentication

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,12 +14,22 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 	try {
 		const authenticateUseCase = makeAuthenticateUseCase();
 
-		await authenticateUseCase.execute({
+		const { user } = await authenticateUseCase.execute({
 			email,
 			password
 		});
+
+		const token = await reply.jwtSign({}, {
+			sign: {
+				sub: user.id
+			}
+		});
+
+		return reply.status(200).send({
+			token
+		});
 	} catch (error) {
-		if(error instanceof InvalidCredentialsError) {
+		if (error instanceof InvalidCredentialsError) {
 			return reply.status(401).send({
 				message: error.message
 			});
@@ -27,6 +37,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
 		throw error;
 	}
-
-	return reply.status(200).send();
 }
