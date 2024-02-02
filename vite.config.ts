4446fc6ec413c6f20b7a218f7748ba7bfdaf6fb3
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-02-02 16:37:00 -0300

    controllers-tests-e2e - feat: finished some more controllers and tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins:[tsConfigPaths()]
+	plugins:[tsConfigPaths()],
+	test: {
+		environmentMatchGlobs: [['src/http/controllers/**', 'prisma/vitest-enviroment-prisma']],
+		dir: 'src', // Essa linha
+	},
 });
